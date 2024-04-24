d38f23cff89bfca92f36458a9591ccd3b7fb8d73
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2024-04-24 12:56:28 -0600

    :lipstick: Adds css files for PrimeReact

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api'
 import App from './App.tsx'
+import "primereact/resources/themes/viva-light/theme.css"
+import "primereact/resources/primereact.min.css"
+import "primeflex/primeflex.css"
+import "primeicons/primeicons.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
